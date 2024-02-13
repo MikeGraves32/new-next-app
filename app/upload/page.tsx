e28592f317303581d189376ca0ecb6f2b5f6d8c6
MikeGraves32 <93281224+MikeGraves32@users.noreply.github.com>
2024-02-13 12:12:42 -0600

    First Next App

---
+++ app/upload/page.tsx
@@ -0,0 +1,20 @@
+'use client';
+import React from 'react';
+import { CldUploadWidget } from 'next-cloudinary';
+
+const UploadPage = () => {
+  return (
+    <CldUploadWidget uploadPreset='wbhhcrjp'
+        onUpload={(result, widget) =>{
+            console.log(result);
+        }}>
+        {({open})=> 
+        <button 
+        className='btn btn-primary'
+        onClick={() => open()}
+        >Upload</button>}
+    </CldUploadWidget>
+  )
+}
+
+export default UploadPage
